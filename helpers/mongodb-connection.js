1cc82d33de148754961fc2f4e7252ade874912ee
Kapil Mundada <kapilmundada21@gmail.com>
2023-03-17 11:00:51 +0000

    including mongodb

---
+++ helpers/mongodb-connection.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv")
+dotenv.config();
+
+// const mongoAtlasUri = process.env.MONGO_URI
+const mongoAtlasUri = "mongodb+srv://kapilmundada21@gmail.com:Kapil@123@atlascluster.beoc0fg.mongodb.net/?retryWrites=true&w=majority"
+
+const mongooseConnection = async () => {
+    try {
+        // Connect to the MongoDB cluster
+        await mongoose.set("strictQuery", false);
+        await mongoose.connect(
+            mongoAtlasUri,
+            { useNewUrlParser: true, useUnifiedTopology: true },
+            () => console.log("Mongoose is connected"),
+        );
+    } catch (e) {
+        console.log("could not connect");
+    }
+    const dbConnection = mongoose.connection;
+    dbConnection.on("error", (err) => console.log(`Connection error ${err}`));
+    dbConnection.once("open", () => console.log("Connected to DB!"));
+}
+module.exports = mongooseConnection;
